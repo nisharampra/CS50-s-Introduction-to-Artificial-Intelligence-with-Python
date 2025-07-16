4b6cc5da0bbb070fa45ace1e4f39d0b647458352
nisharampra <115043583+nisharampra@users.noreply.github.com>
2025-07-16 21:24:21 +0800

    Add project 6 folder

---
+++ project 6/parser/parser.py
@@ -0,0 +1,83 @@
+import nltk
+import sys
+
+TERMINALS = """
+Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
+Adv -> "down" | "here" | "never"
+Conj -> "and" | "until"
+Det -> "a" | "an" | "his" | "my" | "the"
+N -> "armchair" | "companion" | "day" | "door" | "hand" | "he" | "himself"
+N -> "holmes" | "home" | "i" | "mess" | "paint" | "palm" | "pipe" | "she"
+N -> "smile" | "thursday" | "walk" | "we" | "word"
+P -> "at" | "before" | "in" | "of" | "on" | "to"
+V -> "arrived" | "came" | "chuckled" | "had" | "lit" | "said" | "sat"
+V -> "smiled" | "tell" | "were"
+"""
+
+NONTERMINALS = """
+S -> NP VP | S Conj S
+
+NP -> Det NP | Det Nominal | Nominal
+Nominal -> Adj Nominal | N
+
+VP -> V | V NP | V NP PP | V PP | VP Conj VP
+
+PP -> P NP
+"""
+
+
+grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
+parser = nltk.ChartParser(grammar)
+
+
+def main():
+
+    # If filename specified, read sentence from file
+    if len(sys.argv) == 2:
+        with open(sys.argv[1]) as f:
+            s = f.read()
+
+    # Otherwise, get sentence as input
+    else:
+        s = input("Sentence: ")
+
+    # Convert input into list of words
+    s = preprocess(s)
+
+    # Attempt to parse sentence
+    try:
+        trees = list(parser.parse(s))
+    except ValueError as e:
+        print(e)
+        return
+    if not trees:
+        print("Could not parse sentence.")
+        return
+
+    # Print each tree with noun phrase chunks
+    for tree in trees:
+        tree.pretty_print()
+
+        print("Noun Phrase Chunks")
+        for np in np_chunk(tree):
+            print(" ".join(np.flatten()))
+
+
+def preprocess(sentence):
+    words = nltk.word_tokenize(sentence)
+    # Return lowercase words that have at least one alphabetic character
+    return [word.lower() for word in words if any(c.isalpha() for c in word)]
+
+def np_chunk(tree):
+    chunks = []
+    for subtree in tree.subtrees():
+        if subtree.label() == "NP":
+            # Check if this NP contains other NP subtrees inside
+            if not list(subtree.subtrees(lambda t: t != subtree and t.label() == "NP")):
+                chunks.append(subtree)
+    return chunks
+
+
+
+if __name__ == "__main__":
+    main()
